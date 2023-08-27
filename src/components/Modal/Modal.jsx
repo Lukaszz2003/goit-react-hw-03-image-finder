270c8ddbc90d6074a6b8b6535dda65e057d308fe
Łukasz Paradowski <123119119+Lukaszz2003@users.noreply.github.com>
2023-08-27 21:43:10 +0200

    modal

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,1 +1,41 @@
+import React, { Componet } from 'react';
+import css from './Modal.module.css';
+import PropTypes from 'prop-types';
 
+class Modal extends Componet {
+  componentDidMount() {
+    window.addEventListener('keydown', this.handleKeyDown);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('keydown', this.handleKeyDown);
+  }
+
+  handleKeyDown = event => {
+    if (event.code === 'Escape') {
+      this.props.onClose();
+    }
+  };
+
+  handleCose = event => {
+    if (event.target === event.currentTarget) {
+      this.props.onClose();
+    }
+  };
+
+  render() {
+    return (
+      <div className={css.Overlay} onClick={this.handleClose}>
+        <div className={css.Modal}>
+          <img src={this.props.image.largeImageURL} alt="" />
+        </div>
+      </div>
+    );
+  }
+}
+
+Modal.propTypes = {
+  image: PropTypes.object.isRequired,
+  onClose: PropTypes.func.isRequired,
+};
+export default Modal;
