d99f3da42507335c073a2eb20675bca09841b607
Łukasz Paradowski <123119119+Lukaszz2003@users.noreply.github.com>
2023-08-27 21:48:09 +0200

    Loader

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,1 +1,22 @@
+import React from 'react';
+import { ThreeDots } from 'react-loader-spinner';
+import css from './Loader.module.css';
 
+class LoaderSpinner extends React.Component {
+  render() {
+    return (
+      <div className={css.Loader}>
+        <ThreeDots
+          height="60"
+          width="80"
+          radius="16"
+          color="#72f309"
+          ariaLabel="three-dots-loading"
+          visible={true}
+        />
+      </div>
+    );
+  }
+}
+
+export default LoaderSpinner;
